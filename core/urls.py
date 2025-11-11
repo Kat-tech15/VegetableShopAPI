fd9a91b1b43e7af2a8793f9ff4e6c9aa7c0ff33b
Kat-tech15 <kelvinkatwai@gmail.com>
2025-11-11 15:23:37 +0300

    modified core urls to implement documentation

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+shema_view = get_schema_view(
+    openapi.Info(
+        title= "VegetableShop API", 
+        default_version="v1", 
+        description="API documentation for Vegetable and Order endpoints",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="kelvinkatwai@gmail.com")
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('api/vegetables/', include('vegetables.urls')),
     path('api/orders/', include('orders.urls')),
+    path('swagger/', shema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', shema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
